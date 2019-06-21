dc78477e2205a86969572f4bc1aa55edd39c6e1c
jkingz <jfkrule@gmail.com>
2019-06-21 19:45:30 +0800

    js declaring variables

---
+++ main.js
@@ -0,0 +1,5 @@
+// assigning variables using let & const
+
+let age = 27; // << the value of this can be change
+const age2 = 30; // << this is a constant value. it can never be change
+console.log(age, age2, "ages");
