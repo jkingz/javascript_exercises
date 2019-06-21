2d588358b323ab86494a868328ba739bca3d3722
jkingz <jfkrule@gmail.com>
2019-06-21 23:16:00 +0800

    Javascript switch case

--- main.js
+++ main.js
@@ -118,7 +118,22 @@ const todoTextIsCompleted = todos
 console.log(todoTextIsCompleted);
 
 // ternary operator sample
-const x = 11;
+const x = 1;
 
-const color = x > 10 ? "red" : "green";
+const color = x > 10 ? "red" : "blue";
 console.log(color);
+
+const color1 = "green";
+
+// switch case
+switch (color1) {
+  case "red":
+    console.log("color is red");
+    break;
+  case "blue":
+    console.log("color is blue");
+    break;
+  default:
+    console.log("color is not red or blue");
+    break;
+}
