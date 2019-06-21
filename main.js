8096d6bf5dd1363e34037126e82f23f4bf0f6566
jkingz <jfkrule@gmail.com>
2019-06-21 19:47:43 +0800

    js string number boolean null undefined

--- main.js
+++ main.js
@@ -3,3 +3,12 @@
 let age = 27; // << the value of this can be change
 const age2 = 30; // << this is a constant value. it can never be change
 console.log(age, age2, "ages");
+
+// String, Number, Boolean, null, undefined
+const name = "jon";
+const age3 = "20";
+const a = 12;
+const b = true;
+const c = undefined;
+const z;
+
