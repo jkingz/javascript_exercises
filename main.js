b704c5b054f62393f1c58f9b5a4217ea3d6a540a
jkingz <jfkrule@gmail.com>
2019-06-21 19:53:36 +0800

    Concatenating String old way and es6 way template string

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 // assigning variables using let & const
 
-let age = 27; // << the value of this can be change
+let age1 = 27; // << the value of this can be change
 const age2 = 30; // << this is a constant value. it can never be change
-console.log(age, age2, "ages");
+console.log(age1, age2, "ages");
 
 // String, Number, Boolean, null, undefined
 const name = "jon";
-const age3 = "20";
+const age = "20";
 const a = 12;
 const b = true;
 const c = undefined;
-const z;
+let z;
 
+// Concatenation the old way
+console.log("My name is" + name + " and I am " + age);
+
+// ES6 Template String
+console.log(`My name is ${name} and I am ${age}`);
+
+const hello = `My name is ${name} and I am ${age}`;
+console.log(hello);
